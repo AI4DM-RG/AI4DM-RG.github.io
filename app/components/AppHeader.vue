7c7cb85cc310703221851172edcb6768e987956d
Omid Reza Heidari <omid.orh@gmail.com>
2026-05-05 18:57:18 -0400

    fix responsive issue

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -2,16 +2,18 @@
 </script>
 
 <template>
-  <div class="flex ps-4 pe-4 justify-between  mt-2 mb-2">
+  <div class="flex flex-wrap gap-2 ps-4 pe-4 justify-between items-center mt-2 mb-2">
     <NuxtLink class="title text-2xl font-semibold tracking-tight" to="/">
       AI4DM-RG
     </NuxtLink>
-    <div class="flex gap-2 justify-end">
+    <div class="flex gap-2 justify-end items-center">
       <UButton icon="i-lucide-newspaper" size="md" color="neutral" variant="soft"
-               to="https://forms.gle/Xb9qPZUy3iot4pcVA"
-               :label="undefined"
-               class="md:[&_span]:inline">
+               to="https://forms.gle/Xb9qPZUy3iot4pcVA">
         <span class="hidden md:inline">Subscribe to our Newsletter</span>
+      </UButton>
+      <UButton icon="i-lucide-user" size="md" color="neutral" variant="soft"
+               to="/about">
+        <span class="hidden md:inline">About Us</span>
       </UButton>
     </div>
   </div>
